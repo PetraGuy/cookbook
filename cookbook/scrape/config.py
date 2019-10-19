dfba8af13f513a913493e140939e62375f71a616
PetraGuy <petra.guy.uk@gmail.com>
2019-10-19 12:59:48 +0100

    first commit

---
+++ cookbook/scrape/config.py
@@ -0,0 +1,5 @@
+
+start_page_url = r'https://www.metoffice.gov.uk/research/climate/maps-and-data/historic-station-data'
+data_stub_url = r'https://www.metoffice.gov.uk/pub/data/weather/uk/climate/stationdata/{station_name}.txt'
+station_name_list = ['aberporthdata', 'armaghdata', 'ballypatrickdata']
+
---
+++ cookbook/scrape/main.py
@@ -0,0 +1,48 @@
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+from config import start_page_url, data_stub_url, station_name_list
+
+def simple_get(url):
+    """
+    Attempts to get the content at `url` by making an HTTP GET request.
+    If the content-type of response is some kind of HTML/XML, return the
+    text content, otherwise return None.
+    """
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+
+
+def is_good_response(resp):
+    """
+    Returns True if the response seems to be HTML, False otherwise.
+    """
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200
+            and content_type is not None
+            and content_type.find('html') > -1)
+
+
+def log_error(e):
+    """
+    It is always a good idea to log errors.
+    This function just prints them, but you can
+    make it do anything.
+    """
+    print(e)
+
+
+response = simple_get(start_page_url)
+html = BeautifulSoup(response, 'html.parser')
+station_name_tags = html.find_all('option')
+station_names = [tag.contents for tag in station_name_tags]
+pass
